cd29737d4dcdf4fad9ee4bb984210157eb90ca74
Toowaa <brahanax@gmail.com>
2025-04-01 11:09:02 -0500

    feat(api): enhance GetBuses and GetBus functions with error handling

--- api/Routes.tsx
+++ api/Routes.tsx
@@ -1,19 +1,26 @@
 import { Pageable } from "@/interface/interface";
 
-const authHeader = 'Basic ' + btoa(
-    `${process.env.NEXT_PUBLIC_USER}:${process.env.NEXT_PUBLIC_PASSWORD}`
-  );
-export const GetBuses = async ():Promise<Pageable> => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/bus`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: authHeader,
-    },
-  });
+const authHeader =
+  "Basic " +
+  btoa(`${process.env.NEXT_PUBLIC_USER}:${process.env.NEXT_PUBLIC_PASSWORD}`);
+export const GetBuses = async (): Promise<Pageable> => {
 
-  const data :Pageable= await response.json();
-  
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/bus`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: authHeader,
+      },
+    }
+  );
+  if (!response.ok) {
+    throw new Error("Error al obtener el datos");
+  }
+  const data: Pageable = await response.json();
+
   return data;
 };
 
@@ -21,6 +28,9 @@ export const GetBus = async (id: number) => {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/buses/${id}`
   );
+  if (!response.ok) {
+    throw new Error("Error al obtener id del bus");
+  }
   const data = await response.json();
   return data;
 };
